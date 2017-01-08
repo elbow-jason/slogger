615bd116b0b5ba8dc42bcb261a30e280e8a9ca55
Jason Goldberger <jlgoldb2@asu.edu>
2017-01-08 02:35:18 -0700

    ready for publish to hex

--- mix.exs
+++ mix.exs
@@ -9,6 +9,8 @@ defmodule Slogger.Mixfile do
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps(),
+      description: description(),
+      package: package(),
     ]
   end
 
@@ -36,6 +38,27 @@ defmodule Slogger.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+       {:ex_doc, ">= 0.0.0", only: :dev},
+    ]
   end
+
+  defp description do
+    """
+    Slogger is a simple logger that allows flexible, and easily customizable, module-level control of logging.
+    """
+  end
+
+  defp package do
+    [# These are the default files included in the package
+      name: :slogger,
+      files: ["lib", "mix.exs", "README*", "LICENSE*",],
+      maintainers: ["Jason Goldberger"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/elbow-jason/slogger",
+      }
+    ]
+   end
+
 end
