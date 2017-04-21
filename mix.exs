21662237fd8e5fb6c8dd1c6c8b88ea4222fb791c
Jason Goldberger <jlgoldb2@asu.edu>
2017-04-21 15:54:42 -0700

    bumped version to 0.1.6

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Slogger.Mixfile do
   def project do
     [
       app: :slogger,
-      version: "0.1.5",
+      version: "0.1.6",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
