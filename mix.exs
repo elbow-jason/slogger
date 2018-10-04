2b98252cb55342559e1324788ed1eb8ebcf906b5
Jason Goldberger <jlgoldb2@asu.edu>
2018-10-04 01:11:37 -0700

    removed test/support elixirc_path for dev

--- mix.exs
+++ mix.exs
@@ -21,8 +21,9 @@ defmodule Slogger.Mixfile do
     ]
   end
 
-  defp elixirc_paths(env) when env in [:test, :dev], do: ["lib", "test/support"]
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
+
   def application do
     [
       applications: apps()
